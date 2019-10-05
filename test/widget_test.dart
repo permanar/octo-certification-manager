7ce833a3e6797a281236dae97d03d21ccff46b5e
Richie Permana <richie.permana@gmail.com>
2019-10-05 16:23:55 +0800

    Adding new testing on Onboarding Page

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,20 +11,21 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:bisma_certification/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Onboarding screen reach the end page',
+      (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    expect(find.text('Next'), findsOneWidget);
+    expect(find.text('Get Started'), findsNothing);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    await tester.tap(find.byIcon(Icons.arrow_forward));
+    await tester.pumpAndSettle();
+    await tester.tap(find.byIcon(Icons.arrow_forward));
+    await tester.pumpAndSettle();
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.text('Skip'), findsNothing);
+    expect(find.text('Get Started'), findsOneWidget);
+    expect(find.text('Next'), findsNothing);
   });
 }
