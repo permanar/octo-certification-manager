fa656624c843a5f80c0bfaf9690c1cef6c7e7695
Richie Permana <richie.permana@gmail.com>
2019-11-21 21:51:43 +0800

    Improving Home Page Screen

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,8 +1,9 @@
-import 'package:bisma_certification/src/pages/login_page.dart';
+import 'package:bisma_certification/src/pages/home_content.dart';
+import 'package:bisma_certification/src/pages/profile_page.dart';
 import 'package:bisma_certification/src/utils/shared_prefs.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
-import 'package:flutter_swiper/flutter_swiper.dart';
 import 'package:onesignal_flutter/onesignal_flutter.dart';
 
 class HomePage extends StatefulWidget {
@@ -14,11 +15,15 @@ class _HomePageState extends State<HomePage> {
   SharedPrefs prefs = SharedPrefs();
   int _currentPage = 0;
   bool _isLogin;
+  List<Widget> _widgetOptions = <Widget>[
+    HomeContent(),
+    ProfilePage(),
+  ];
 
   @override
   void initState() {
     super.initState();
-    _initSP();
+    // _initSP();
     initOneSignal();
   }
 
@@ -57,21 +62,6 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
-  // Checking if login not proceed at the very beginning, go directly to the login page
-  // Still not sure about this idea but, let's give it a shot
-  _initSP() {
-    prefs.read('login').then((val) {
-      _isLogin = val;
-      if (_isLogin != true) {
-        print("mantaapp if condt true homepage!!! => $_isLogin)}");
-        setState(() {
-          Navigator.of(context).pushReplacement(
-              MaterialPageRoute(builder: (context) => LoginPage()));
-        });
-      }
-    });
-  }
-
   _logoutProcess() {
     setState(() {
       prefs.delete('login').then((val) {
@@ -91,16 +81,16 @@ class _HomePageState extends State<HomePage> {
     print("uda di build $_isLogin");
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          "Bisma Certification",
-          style: TextStyle(
-            color: Colors.red[300],
-          ),
-        ),
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-      ),
+      // appBar: AppBar(
+      //   title: Text(
+      //     "Bisma Certification",
+      //     style: TextStyle(
+      //       color: Colors.red[300],
+      //     ),
+      //   ),
+      //   backgroundColor: Colors.transparent,
+      //   elevation: 0,
+      // ),
       // body: ListView.builder(
       //   padding: EdgeInsets.symmetric(vertical: 16.0),
       //   itemBuilder: (BuildContext context, int index) {
@@ -112,121 +102,15 @@ class _HomePageState extends State<HomePage> {
       //     }
       //   },
       // ),
-      body: Container(
-        child: Column(
-          // overflow: Overflow.visible,
-          children: [
-            Container(
-              height: 150,
-              child: Swiper(
-                itemBuilder: (BuildContext context, int index) {
-                  return new Image.network(
-                    "http://via.placeholder.com/350x150",
-                    fit: BoxFit.fill,
-                  );
-                },
-                itemCount: 5,
-                viewportFraction: 0.8,
-                pagination: SwiperPagination(
-                  alignment: Alignment.bottomCenter,
-                  builder: SwiperPagination.dots,
-                ),
-                control: SwiperControl(
-                  iconNext: Icons.navigate_next,
-                  iconPrevious: Icons.navigate_before,
-                ),
-
-                scale: 0.9,
-                autoplay: true,
-                // duration: 3000,
-                fade: .2,
-                loop: false,
-                curve: Curves.ease,
-                // layout: SwiperLayout.STACK,
-                itemWidth: 300,
-                itemHeight: 150,
-              ),
-            ),
-            SingleChildScrollView(
-              child: Padding(
-                padding: const EdgeInsets.only(left: 15, right: 15, bottom: 30),
-                child: Column(
-                  children: <Widget>[
-                    Container(
-                      width: 200,
-                      child: Card(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(15.0),
-                        ),
-                        color: Colors.pink,
-                        elevation: 10,
-                        child: Column(
-                          mainAxisSize: MainAxisSize.min,
-                          children: <Widget>[
-                            const ListTile(
-                              leading: Icon(Icons.album, size: 70),
-                              title: Text('Heart Shaker',
-                                  style: TextStyle(color: Colors.white)),
-                              subtitle: Text('TWICE',
-                                  style: TextStyle(color: Colors.white)),
-                            ),
-                            ButtonTheme.bar(
-                              child: ButtonBar(
-                                children: <Widget>[
-                                  FlatButton(
-                                    child: const Text('Edit',
-                                        style: TextStyle(color: Colors.white)),
-                                    onPressed: () {
-                                      Navigator.of(context).push(
-                                        MaterialPageRoute(
-                                          builder: (BuildContext build) =>
-                                              MySecondPage(
-                                            todos: List.generate(
-                                              10,
-                                              (i) => i,
-                                            ),
-                                          ),
-                                        ),
-                                      );
-                                    },
-                                  ),
-                                  FlatButton(
-                                    child: const Text('Delete',
-                                        style: TextStyle(color: Colors.white)),
-                                    onPressed: () {
-                                      Navigator.of(context).push(
-                                        MaterialPageRoute(
-                                          builder: (BuildContext build) =>
-                                              MySecondPage(
-                                            todos: List.generate(
-                                              10,
-                                              (i) => i,
-                                            ),
-                                          ),
-                                        ),
-                                      );
-                                    },
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                    Text("data"),
-                  ],
-                ),
-              ),
-            )
-          ],
-        ),
-      ),
+      body: _widgetOptions.elementAt(_currentPage),
       extendBody: true,
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
-        onPressed: _logoutProcess,
+        onPressed: () {
+          Navigator.of(context)
+              .push(MaterialPageRoute(builder: (context) => ProfilePage()));
+        },
       ),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _currentPage,
@@ -234,11 +118,10 @@ class _HomePageState extends State<HomePage> {
           setState(() {
             _currentPage = index;
           });
-          print(index);
-          if (index == 1) {
-            Navigator.of(context).pushReplacement(
-                MaterialPageRoute(builder: (context) => LoginPage()));
-          }
+          // if (index == 1) {
+          //   Navigator.of(context).pushReplacement(
+          //       MaterialPageRoute(builder: (context) => ProfilePage()));
+          // }
         },
         items: [
           BottomNavigationBarItem(
@@ -283,37 +166,6 @@ class _HomePageState extends State<HomePage> {
         decoration: BoxDecoration(
           color: Colors.grey,
           borderRadius: BorderRadius.all(Radius.circular(4.0)),
-        ),
-      ),
-    );
-  }
-}
-
-class MySecondPage extends StatefulWidget {
-  final List<int> todos;
-
-  const MySecondPage({Key key, this.todos}) : super(key: key);
-
-  @override
-  _MySecondPageState createState() => _MySecondPageState();
-}
-
-class _MySecondPageState extends State<MySecondPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text("datas"),
-      ),
-      body: Container(
-        child: ListView.builder(
-          itemCount: widget.todos.length,
-          itemBuilder: (BuildContext context, int id) {
-            return ListTile(
-              title: Text("${widget.todos[id]}"),
-            );
-          },
         ),
       ),
     );
