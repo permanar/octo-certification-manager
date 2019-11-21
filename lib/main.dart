13e780d4cd133f7788b3a3d1a393fb26fb470213
Richie Permana <richie.permana@gmail.com>
2019-11-21 21:38:52 +0800

    Modifying code and adding Future

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,25 @@
+import 'dart:io';
+
 import 'package:bisma_certification/src/pages/home_page.dart';
 import 'package:bisma_certification/src/pages/onboarding/onboarding_page.dart';
+import 'package:bisma_certification/src/utils/shared_prefs.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  return runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  final SharedPrefs prefs = SharedPrefs();
+
+  Future<bool> _initBro() async {
+    var redirect = await prefs.readBool("onboard") ?? false;
+    print("main => $redirect");
+    // return Future.delayed(Duration(seconds: 7), () => redirect);
+    return redirect;
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -14,8 +28,29 @@ class MyApp extends StatelessWidget {
         fontFamily: 'CM Sans Serif',
         platform: TargetPlatform.android,
       ),
-      home: OnBoardingPage(),
-      // home: HomePage(),
+      home: FutureBuilder(
+        future: _initBro(),
+        builder: (context, AsyncSnapshot<bool> snapshot) {
+          if (snapshot.hasData) {
+            print("main inside => ${snapshot.data}");
+            return snapshot.data ? HomePage() : OnBoardingPage();
+          } else {
+            return Scaffold(
+              body: Container(
+                child: Center(
+                  // child: SpinKitFadingFour(
+                  //   color: Colors.black,
+                  //   controller: AnimationController(
+                  //     duration: Duration(seconds: 2400),
+                  //   ),
+                  // ),
+                  child: CircularProgressIndicator(),
+                ),
+              ),
+            );
+          }
+        },
+      ),
       debugShowCheckedModeBanner: false,
     );
   }
