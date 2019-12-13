0d8bc6664da6e7dc96cb926d05b3622b66357cec
Richie Permana <richie.permana@gmail.com>
2019-12-13 15:01:31 +0800

    Improving Checkout Page

--- lib/src/pages/checkout_page.dart
+++ lib/src/pages/checkout_page.dart
@@ -1,8 +1,11 @@
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:bisma_certification/src/pages/payment_page.dart';
 import 'package:bisma_certification/src/pages/profile_page.dart';
 import 'package:bisma_certification/src/utils/page_transition.dart';
 import 'package:bisma_certification/src/utils/shared_prefs.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_money_formatter/flutter_money_formatter.dart';
 
 class CheckoutPage extends StatefulWidget {
   @override
@@ -11,71 +14,13 @@ class CheckoutPage extends StatefulWidget {
 
 class _CheckoutPageState extends State<CheckoutPage> {
   SharedPrefs prefs = SharedPrefs();
-
-  Future<Widget> buildShopCart() async {
-    List cart = await prefs.readCart("cart");
-    // print("heeyyoooo length => ${cart.length}\nheeyyoooo isinyaaaa => $cart\n");
-
-    return cart.length > 0
-        ? ListView.builder(
-            itemCount: cart.length,
-            itemBuilder: (context, id) {
-              return Dismissible(
-                onDismissed: (direction) {
-                  setState(() {
-                    prefs.deleteCart(id);
-                  });
-                },
-                direction: DismissDirection.endToStart,
-                key: ValueKey(cart[id][1]),
-                background: Container(
-                  color: Colors.grey[200],
-                ),
-                child: ListTile(
-                  contentPadding: EdgeInsets.symmetric(horizontal: 0),
-                  leading: ClipRRect(
-                    borderRadius: BorderRadius.circular(8),
-                    child: CachedNetworkImage(
-                      imageUrl: cart[id][0],
-                      placeholder: (context, url) => Center(
-                        child: Container(
-                          height: 50,
-                          width: 50,
-                          child: CircularProgressIndicator(),
-                        ),
-                      ),
-                      errorWidget: (context, url, error) => Center(
-                        child: Icon(
-                          Icons.error,
-                        ),
-                      ),
-                    ),
-                  ),
-                  title: Text(cart[id][1]),
-                  subtitle: Row(
-                    children: <Widget>[
-                      Text("Quantity: 1"),
-                      SizedBox(width: 20),
-                      Text("Rp. ${cart[id][2]}")
-                    ],
-                  ),
-                  trailing: Icon(Icons.close),
-                ),
-              );
-            },
-          )
-        : Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text("You have no items. Let's buy something! "),
-              Icon(Icons.timelapse)
-            ],
-          );
-  }
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
+      bottomNavigationBar: bottomNavDetailPage(context),
       appBar: AppBar(
         leading: BackButton(
           color: Colors.black45,
@@ -89,15 +34,19 @@ class _CheckoutPageState extends State<CheckoutPage> {
         elevation: 0,
       ),
       body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
         child: Container(
+          height: MediaQuery.of(context).size.height,
           child: ListView(
             children: <Widget>[
               Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                  Container(
-                    height: 255,
+                  ConstrainedBox(
+                    constraints: BoxConstraints(
+                      minHeight: 150,
+                      maxHeight: 250,
+                    ),
                     child: FutureBuilder(
                       initialData: CircularProgressIndicator(),
                       future: buildShopCart(),
@@ -113,6 +62,7 @@ class _CheckoutPageState extends State<CheckoutPage> {
                   Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: <Widget>[
+                      SizedBox(height: 20),
                       Row(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: <Widget>[
@@ -204,10 +154,291 @@ class _CheckoutPageState extends State<CheckoutPage> {
                         ),
                       ),
                     ],
-                  )
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Divider(),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Text("Subtotal"),
+                          FutureBuilder(
+                            future: prefs.readCart('cart'),
+                            builder: (context, snapshot) {
+                              if (snapshot.hasData) {
+                                int total = 0;
+
+                                for (var item in snapshot.data) {
+                                  print(item);
+                                  total +=
+                                      int.parse(item[item.keys.first]['total']);
+                                }
+
+                                FlutterMoneyFormatter fmf =
+                                    FlutterMoneyFormatter(
+                                  amount: double.parse(total.toString()),
+                                  settings: MoneyFormatterSettings(
+                                    symbol: "IDR",
+                                    thousandSeparator: ".",
+                                    decimalSeparator: ",",
+                                    symbolAndNumberSeparator: " ",
+                                  ),
+                                );
+
+                                return Text("Rp. ${fmf.output.nonSymbol}");
+                              } else {
+                                return CircularProgressIndicator();
+                              }
+                            },
+                          )
+                        ],
+                      ),
+                      SizedBox(height: 5),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Text("Discount"),
+                          Text("-"),
+                        ],
+                      ),
+                      Divider(),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Text(
+                            "Total",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                            ),
+                          ),
+                          FutureBuilder(
+                            future: prefs.readCart('cart'),
+                            builder: (context, snapshot) {
+                              if (snapshot.hasData) {
+                                int total = 0;
+
+                                for (var item in snapshot.data) {
+                                  print(item);
+                                  total +=
+                                      int.parse(item[item.keys.first]['total']);
+                                }
+
+                                FlutterMoneyFormatter fmf =
+                                    FlutterMoneyFormatter(
+                                  amount: double.parse(total.toString()),
+                                  settings: MoneyFormatterSettings(
+                                    symbol: "IDR",
+                                    thousandSeparator: ".",
+                                    decimalSeparator: ",",
+                                    symbolAndNumberSeparator: " ",
+                                  ),
+                                );
+
+                                return Text("Rp. ${fmf.output.nonSymbol}");
+                              } else {
+                                return CircularProgressIndicator();
+                              }
+                            },
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
                 ],
               )
             ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future<Widget> buildShopCart() async {
+    List cart = await prefs.readCart("cart");
+    List<String> cartKey = [];
+
+    for (var item in cart) {
+      cartKey.add(item.keys.first.toString());
+    }
+    // print("heeyyoooo length => ${cart.length}\nheeyyoooo isinyaaaa => $cart\n");
+
+    return cart.length > 0
+        ? ListView.builder(
+            itemCount: cart.length,
+            itemBuilder: (context, id) {
+              return Dismissible(
+                onDismissed: (direction) {
+                  setState(() {
+                    prefs.deleteCart(id);
+                  });
+                },
+                direction: DismissDirection.endToStart,
+                key: ValueKey(cart[id][cartKey[id]]['name']),
+                background: Container(
+                  color: Colors.grey[200],
+                ),
+                child: ListTile(
+                  contentPadding: EdgeInsets.symmetric(horizontal: 0),
+                  leading: CachedNetworkImage(
+                    imageUrl: cart[id][cartKey[id]]['image'],
+                    placeholder: (context, url) => Center(
+                      child: Container(
+                        height: 50,
+                        width: 50,
+                        child: CircularProgressIndicator(),
+                      ),
+                    ),
+                    errorWidget: (context, url, error) => Center(
+                      child: Icon(
+                        Icons.error,
+                      ),
+                    ),
+                  ),
+                  title: AutoSizeText(
+                    cart[id][cartKey[id]]['name'],
+                    maxLines: 2,
+                  ),
+                  isThreeLine: true,
+                  subtitle: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      FutureBuilder(
+                        future: prefs.readCart('cart'),
+                        builder: (context, snapshot) {
+                          if (snapshot.hasData) {
+                            FlutterMoneyFormatter fmf = FlutterMoneyFormatter(
+                              amount: double.parse(
+                                  snapshot.data[id][cartKey[id]]['price']),
+                              settings: MoneyFormatterSettings(
+                                symbol: "IDR",
+                                thousandSeparator: ".",
+                                decimalSeparator: ",",
+                                symbolAndNumberSeparator: " ",
+                              ),
+                            );
+
+                            return AutoSizeText(
+                              "Rp. ${fmf.output.nonSymbol}",
+                              maxLines: 1,
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                              ),
+                            );
+                          } else {
+                            return CircularProgressIndicator();
+                          }
+                        },
+                      ),
+                      AutoSizeText(
+                        "Sistem Informasi",
+                        maxLines: 1,
+                      ),
+                    ],
+                  ),
+                  trailing: Container(
+                    width: 113,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: <Widget>[
+                        ButtonTheme(
+                          minWidth: 15,
+                          height: 10,
+                          child: RaisedButton(
+                            onPressed: () {
+                              setState(() {
+                                if (cart[id][cartKey[id]]['qty'] <= 1) {
+                                  return;
+                                }
+                                prefs.decreaseCart("cart", "LSPP");
+                              });
+                            },
+                            highlightColor: Colors.transparent,
+                            elevation: 0,
+                            color: Color(0xffe2defc),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(10),
+                                bottomLeft: Radius.circular(10),
+                              ),
+                            ),
+                            child: Text(
+                              "-",
+                              style: TextStyle(
+                                fontSize: 16,
+                              ),
+                            ),
+                          ),
+                        ),
+                        Text(
+                          cart[id][cartKey[id]]['qty'].toString(),
+                        ),
+                        ButtonTheme(
+                          minWidth: 15,
+                          height: 10,
+                          child: RaisedButton(
+                            onPressed: () {
+                              setState(() {
+                                prefs.addCart("cart", "LSPP");
+                              });
+                            },
+                            highlightColor: Colors.transparent,
+                            elevation: 0,
+                            color: Color(0xffe2defc),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.only(
+                                topRight: Radius.circular(10),
+                                bottomRight: Radius.circular(10),
+                              ),
+                            ),
+                            child: Text(
+                              "+",
+                              style: TextStyle(
+                                fontSize: 16,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              );
+            },
+          )
+        : Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text("You have no items. Let's buy something! "),
+              Icon(Icons.timelapse)
+            ],
+          );
+  }
+
+  Widget bottomNavDetailPage(context) {
+    return Padding(
+      padding: const EdgeInsets.all(8),
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        height: 40,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15),
+          child: FlatButton(
+            onPressed: () {
+              setState(() {
+                Navigator.of(context).push(
+                  PageTransitionFade(page: PaymentPage()),
+                );
+              });
+            },
+            child: Text(
+              'Pay',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+            color: Color(0xff5E4FD1),
           ),
         ),
       ),
