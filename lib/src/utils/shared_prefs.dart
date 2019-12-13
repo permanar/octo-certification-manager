72cc1874d08c59f73d93dd48749f32ae27d83079
Richie Permana <richie.permana@gmail.com>
2019-12-13 15:01:59 +0800

    Adding some new function and improve the logic

--- lib/src/utils/shared_prefs.dart
+++ lib/src/utils/shared_prefs.dart
@@ -13,16 +13,35 @@ class SharedPrefs {
     return prefs.setBool(key, value);
   }
 
-  Future addCart(String key, value) async {
+  Future addCart(String key, id, {value}) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    List cart;
+    List cart = [];
 
     if (prefs.getString("cart") != null) {
       cart = jsonDecode(prefs.getString("cart"));
-    } else {
-      cart = [];
+      List cartKey = [];
+
+      for (var item in cart) {
+        cartKey.add(item.keys.first);
+      }
+
+      if (cart.isEmpty == false && cartKey.contains(id)) {
+        cart[cartKey.indexOf(id)][id]['qty'] += 1;
+        cart[cartKey.indexOf(id)][id]['total'] = (cart[cartKey.indexOf(id)][id]
+                    ['qty'] *
+                int.parse(cart[cartKey.indexOf(id)][id]['price']))
+            .toString();
+      } else {
+        if (value != null) {
+          print('um, ketambah? => $value');
+          cart.add(value);
+        }
+      }
     }
-    cart.add(value);
+    if (cart.isEmpty || cart == null) {
+      cart = [];
+      cart.add(value);
+    }
 
     print(
         "cart after inser => $cart\ncart after inser encode => ${jsonEncode(cart)}\n");
@@ -42,6 +61,7 @@ class SharedPrefs {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     // return jsonDecode(prefs.getString(key));
     String data = prefs.getString(key);
+    Future.delayed(Duration(seconds: 10));
     return jsonDecode(data);
   }
 
@@ -56,5 +76,31 @@ class SharedPrefs {
     cart.removeAt(id);
 
     return prefs.setString("cart", jsonEncode(cart));
+  }
+
+  Future decreaseCart(String key, String id) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    List cart;
+
+    if (prefs.getString(key) != null) {
+      cart = jsonDecode(prefs.getString(key));
+      List cartKey = [];
+
+      for (var item in cart) {
+        cartKey.add(item.keys.first);
+      }
+
+      if (cart.isEmpty == false &&
+          cart[cartKey.indexOf(id)].containsKey(id) &&
+          cart[cartKey.indexOf(id)][id]['qty'] > 0) {
+        cart[cartKey.indexOf(id)][id]['qty'] -= 1;
+        cart[cartKey.indexOf(id)][id]['total'] =
+            (int.parse(cart[cartKey.indexOf(id)][id]['total']) -
+                    int.parse(cart[cartKey.indexOf(id)][id]['price']))
+                .toString();
+      }
+    }
+
+    return prefs.setString(key, jsonEncode(cart));
   }
 }
